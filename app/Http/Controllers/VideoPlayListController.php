e11664229e53d6a9a8acf8a4cbecc08360380273
Long-Soeng167 <generalpurpose167@gmail.com>
2025-04-27 21:08:15 +0700

    feat: Implement Video Play List functionality with controller, requests, model, policy, factory, migration, and seeder

---
+++ app/Http/Controllers/VideoPlayListController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreVideoPlayListRequest;
+use App\Http\Requests\UpdateVideoPlayListRequest;
+use App\Models\VideoPlayList;
+
+class VideoPlayListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVideoPlayListRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(VideoPlayList $videoPlayList)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(VideoPlayList $videoPlayList)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVideoPlayListRequest $request, VideoPlayList $videoPlayList)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(VideoPlayList $videoPlayList)
+    {
+        //
+    }
+}
