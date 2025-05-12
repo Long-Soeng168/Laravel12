6b4919f25b86488c5d891679f9eff647bafb3191
Long-Soeng168 <generalpurpose167@gmail.com>
2025-05-12 13:41:33 +0700

    Fix positioning of scroll to top button for improved layout consistency

--- resources/js/pages/westernUniversityNew/layout/MyLayout.tsx
+++ resources/js/pages/westernUniversityNew/layout/MyLayout.tsx
@@ -18,7 +18,7 @@ const MyNewLayout = ({ children }: LayoutProps) => {
             {/* Footer */}
             <MyNewFooter />
             {/* Scroll to Top Button */}
-            <a href="#top" className="fixed right-15 md:right-6 bottom-6 rounded-full bg-gray-800 px-4 py-4 text-white shadow-lg hover:bg-blue-900">
+            <a href="#top" className="fixed right-2 bottom-2 md:right-6 md:bottom-6 rounded-full bg-gray-800 px-4 py-4 text-white shadow-lg hover:bg-blue-900">
                 <ArrowUpCircle className='w-7 h-7'/>
             </a>
             {/* End Footer */}
