24980b7e05238074d19224ec4bea0a367175e986
Long-Soeng167 <generalpurpose167@gmail.com>
2026-04-17 21:22:42 +0700

    update compressize image

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -103,7 +103,7 @@ class PageController extends Controller
         if ($image_files) {
             try {
                 foreach ($image_files as $image) {
-                    $created_image_name = ImageHelper::uploadAndResizeImage($image, 'assets/images/pages', 800);
+                    $created_image_name = ImageHelper::uploadAndResizeImage($image, 'assets/images/pages', 1200);
                     PageImage::create([
                         'image' => $created_image_name,
                         'page_id' => $created_project->id,
@@ -194,7 +194,7 @@ class PageController extends Controller
         if ($image_files) {
             try {
                 foreach ($image_files as $image) {
-                    $created_image_name = ImageHelper::uploadAndResizeImage($image, 'assets/images/pages', 800);
+                    $created_image_name = ImageHelper::uploadAndResizeImage($image, 'assets/images/pages', 1200);
                     PageImage::create([
                         'image' => $created_image_name,
                         'page_id' => $page->id,
